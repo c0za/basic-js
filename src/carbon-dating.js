bcf97e98c45f0b4a1de43aa43bd1c3dc18def083
Andrey Maximov <c0za@mail.ru>
2022-10-15 22:22:09 +0300

    feat: solve carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,6 +19,14 @@ const HALF_LIFE_PERIOD = 5730;
  */
 function dateSample(sampleActivity) {
 
+  if (typeof sampleActivity !== 'string' || sampleActivity <= 0 || sampleActivity > 15 || isNaN(sampleActivity)) {
+    return false;
+  }
+  const active = parseFloat(sampleActivity);
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = (Math.log(MODERN_ACTIVITY / active)) / k;
+  return Math.ceil(t);
+
 }
 
 module.exports = {
